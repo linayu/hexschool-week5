f0b165504398a9cb8f1eef4e33bed1e7c0ffea91
lina <m6ruplina@gmail.com>
2020-06-19 01:24:00 +0800

    Update 2020-06-18T17:23:54.835Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
+"use strict";
+
+$(document).ready(function () {
   var check_url = location.pathname;
   console.log(check_url);
+
   if (check_url) {
     $(".nav .nav-link").find(".active").removeClass("active");
-    $('a[href="' + location.pathname + '"]').addClass('active'); 
+    $('a[href="' + location.pathname + '"]').addClass('active');
   }
-
 });
+//# sourceMappingURL=all.js.map
