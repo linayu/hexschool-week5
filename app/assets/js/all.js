a707314a66ddd428cfe06fd577b367619b7d00a7
lina <m6ruplina@gmail.com>
2020-06-19 01:53:29 +0800

    [bug] fix nav-link active error

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  var check_url = location.pathname;
-  console.log(check_url);
-  if (check_url) {
-    $(".nav .nav-link").find(".active").removeClass("active");
-    $('a[href="' + location.pathname + '"]').addClass('active'); 
-  }
+
+  var nav_id = sessionStorage.getItem('nav_id');
+  $('#'+nav_id).addClass('active'); 
+
 
 });
